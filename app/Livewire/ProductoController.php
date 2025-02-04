ec2941139d45f12e7c2ac0bddddd77f2f825dcf4
Franky Mejia <fryk88@gmail.com>
2025-02-04 10:10:25 -0600

    corregir exportar producto indivudal

--- app/Livewire/ProductoController.php
+++ app/Livewire/ProductoController.php
@@ -428,7 +428,7 @@ class ProductoController extends Component
     public function exportarFila($id)
     {
 
-        $dato=Producto::find($id)->with('marca')->with('material')->with('tipo')->with('disenio')->with('sucursales')->first();
+        $dato=Producto::where('id', $id)->with('marca')->with('material')->with('tipo')->with('disenio')->with('sucursales')->first();
 
             $fecha_reporte=Carbon::now()->toDateTimeString();
             $pdf = Pdf::loadView('/livewire/pdf/pdfProducto',['dato' => $dato]);
